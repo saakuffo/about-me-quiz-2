9645d2deec5b45494e16619dec406246913929d8
Stephen Akuffo <slyven@gmail.com>
2016-08-09 04:32:48 +0000

    initial quiz done

--- app/controllers/abouts_controller.rb
+++ app/controllers/abouts_controller.rb
@@ -1,9 +1,21 @@
 class AboutsController < ApplicationController
   def index
-    @test = Project.first
+    @projects = Project.all
   end
 
   def new
-    
+    @projects = Project.new
   end
+
+  def create
+    Project.create(project_params)
+    redirect_to root_path
+  end
+
+  private
+  
+  def project_params
+    params.require(:project).permit(:appname, :languages, :description, :demo, :photolink)
+  end
+
 end
